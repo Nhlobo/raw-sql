db338d47cb4b9f90c98c1d2c56a9f0b18a450608
Nhlovo <137711037+Nhlobo@users.noreply.github.com>
2026-06-25 17:18:58 +0200

    Create EnterpriseAuthApplication.java

---
+++ backend-java/src/main/java/com/kutlwano/auth/EnterpriseAuthApplication.java
@@ -0,0 +1,11 @@
+package com.kutlwano.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAuthApplication.class, args);
+    }
+}
